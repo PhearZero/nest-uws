0b4c274773bfdb5a31be7d3f821e65da758ed2d2
Michael J Feher <github@phearzero.com>
2023-07-07 12:11:20 -0500

    ✨ uWebSocket.js Adapters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { UwsHttpAdapter } from './adapters/uws-http-adapter';
+import { UwsIoAdapter } from './adapters/uws-io-adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new UwsHttpAdapter());
+  const ioAdapter = new UwsIoAdapter(app);
+  app.useWebSocketAdapter(ioAdapter);
   await app.listen(3000);
 }
 bootstrap();
